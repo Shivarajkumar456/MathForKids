91f335bed8819d3246b0ece0745e8faf79b6f35a
shivarajkumar <shivaraj1654@gmail.com>
2022-11-19 19:14:26 +0530

    made responsive

--- JS/div.js
+++ JS/div.js
@@ -13,6 +13,7 @@ function generate_equation(){
     var switchanswers = [];
 
     answer = num1 / num2;
+    answer = answer.toFixed(2);
 
     document.getElementById("num1").innerHTML = num1;
     document.getElementById("num2").innerHTML = num2;
--- JS/mul.js
+++ JS/mul.js
@@ -1,7 +1,7 @@
 const option1 = document.getElementById("option1");
 const option2 = document.getElementById("option2");
 const option3 = document.getElementById("option3");
-const audio = document.getElementById("myaudio");
+const audio = document.getElementById("myAudio");
 var answer = 0;
 
 function generate_equation(){
